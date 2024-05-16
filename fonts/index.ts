6481a2d7c80752adf34763672db922e6c530c062
Favour Max-Oti <maxotif@gmail.com>
2024-05-16 03:02:06 +0100

    initial commit and application setup

---
+++ fonts/index.ts
@@ -0,0 +1,3 @@
+import { Source_Sans_3 } from "next/font/google";
+
+export const sourceSans = Source_Sans_3({});
---
+++ lib/utils.ts
@@ -0,0 +1,15 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+import axios from "axios"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const fetcher = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+})
